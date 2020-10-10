fe851dcfd9364b65968b701cc2ff592a6a1d9c70
stephen <mqhbeijing@163.com>
2020-10-10 16:55:37 +0800

    定义启动类及banner处理类

---
+++ src/main/java/com/github/rateldesign/common/Banner.java
@@ -0,0 +1,9 @@
+package com.github.rateldesign.common;
+
+/**
+ * @Description: banner处理
+ * @Author stephen
+ * @Date 2020/10/10 4:41 下午
+ */
+public class Banner {
+}
---
+++ src/main/java/com/github/rateldesign/core/SpringApplication.java
@@ -0,0 +1,9 @@
+package com.github.rateldesign.core;
+
+/**
+ * @Description: 启动类
+ * @Author stephen
+ * @Date 2020/10/10 4:53 下午
+ */
+public final class SpringApplication {
+}
